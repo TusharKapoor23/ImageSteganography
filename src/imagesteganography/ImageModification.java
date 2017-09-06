9aac75e99872d432d175a82fd6991ab89615a650
Tushar Kapoor <tusharhappy@gmail.com>
2017-09-06 17:55:22 +0000

    Data Retrieval Fixed

--- src/imagesteganography/ImageModification.java
+++ src/imagesteganography/ImageModification.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import javax.imageio.ImageIO;
 
 /**
@@ -48,13 +49,13 @@ class test {
             for (int j = 0; j < width; j++) {
                 int x = img.getRGB(j, i);
                 pixels[i][j] = Integer.toBinaryString(x);
-                System.out.print(pixels[i][j] + "\t");
+                //System.out.print(pixels[i][j] + "\t");
             }
-            System.out.println("");
+            //System.out.println("");
         }
         //Encryption Part
         AES a = new AES();
-        String str = new String(Files.readAllBytes(Paths.get("input2.txt")));
+        String str = new String(Files.readAllBytes(Paths.get("input3.txt")));
         char[] message = str.toCharArray();
         char[] key = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         int originalLen = message.length;
@@ -66,6 +67,7 @@ class test {
 
         char[] paddedMessage = new char[lenOfPaddedMessage];
         char[] encryptedMessage = new char[lenOfPaddedMessage];
+        char[] newMessage = new char[lenOfPaddedMessage];
         for (int i = 0; i < lenOfPaddedMessage; i++) {
             if (i >= originalLen) {
                 paddedMessage[i] = 0;
@@ -84,6 +86,10 @@ class test {
                 encryptedMessage[i + j] = tmp[j];
             }
         }
+        for (int i = 0; i < encryptedMessage.length; i++) {
+            System.out.print(encryptedMessage[i]);
+        }
+        System.out.println("");
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < encryptedMessage.length; i++) {
             String b = Integer.toBinaryString(encryptedMessage[i]);
@@ -96,13 +102,13 @@ class test {
             s.append(b);
         }
         int counter = 0;
-        System.out.println(s);
+
         //Put Info in image
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 StringBuilder x = new StringBuilder(pixels[i][j]);
-                x.deleteCharAt(x.length() -1);
-                x.deleteCharAt(x.length() -1);
+                x.deleteCharAt(x.length() - 1);
+                x.deleteCharAt(x.length() - 1);
                 if (counter < s.length()) {
                     x.append(s.charAt(counter++));
                     x.append(s.charAt(counter++));
@@ -121,21 +127,40 @@ class test {
             for (int j = 0; j < width; j++) {
                 int x = img.getRGB(j, i);
                 pixels[i][j] = Integer.toBinaryString(x);
-                System.out.print(pixels[i][j] + "\t");
+                //System.out.print(pixels[i][j] + "\t");
             }
-            System.out.println("");
+            //System.out.println("");
         }
         counter = 0;
         StringBuilder sentMessage = new StringBuilder();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if(counter < s.length()){
-                    sentMessage.append(pixels[i][j].charAt(pixels[i][j].length()-2));
-                    sentMessage.append(pixels[i][j].charAt(pixels[i][j].length()-1));
+                if (counter < s.length()) {
+                    sentMessage.append(pixels[i][j].charAt(pixels[i][j].length() - 2));
+                    sentMessage.append(pixels[i][j].charAt(pixels[i][j].length() - 1));
                 }
             }
         }
-        System.out.println(sentMessage); 
-        System.out.println(s);
+        String input = s.toString();
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(input.split("(?<=\\G.{8})")).forEach(st-> sb.append((char) Integer.parseInt(st, 2)));
+        String output = sb.toString();
+        newMessage = output.toCharArray();
+        System.out.println("DECRYPTION");
+
+        for (int i = 0; i < lenOfPaddedMessage; i += 16) {
+            char[] tmp = new char[16];
+            for (int j = 0; j < 16; j++) {
+                tmp[j] = newMessage[i + j];
+            }
+            a.AES_Decrypt(tmp, key);
+            for (int j = 0; j < 16; j++) {
+                newMessage[i + j] = tmp[j];
+            }
+        }
+        for (int i = 0; i < newMessage.length; i++) {
+            System.out.print(newMessage[i]);
+        }
+        System.out.println("");
     }
 }
